5547829c47a777ec95b4ede3481dc33f44114edf
ReginaLin2005 <ReginaLin2005@gmail.com>
2022-11-01 19:20:49 -0700

    this code is extra

---
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,125 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.Pigeon2;
+import frc.robot.Constants.kPorts;
+import frc.robot.Constants.kSwerve;
+import frc.robot.util.SwerveModule;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Swerve extends SubsystemBase {
+    public SwerveDriveOdometry swerveOdometry;
+    public SwerveModule[] mSwerveMods;
+    public Pigeon2 gyro;
+    private final Field2d field;
+
+    private static Swerve instance;
+
+    public static Swerve getInstance() {
+        if (instance == null) {
+            instance = new Swerve();
+        }
+        return instance;
+    }
+
+    private Swerve() {
+        gyro = new Pigeon2(kPorts.PIGEON_ID);
+        gyro.configFactoryDefault();
+        zeroGyro();
+
+        field = new Field2d();
+ 
+        swerveOdometry = new SwerveDriveOdometry(kSwerve.SWERVE_KINEMATICS, getYaw());
+
+        mSwerveMods = new SwerveModule[] {
+                new SwerveModule(0, kSwerve.Mod0.CONSTANTS),
+                new SwerveModule(1, kSwerve.Mod1.CONSTANTS),
+                new SwerveModule(2, kSwerve.Mod2.CONSTANTS),
+                new SwerveModule(3, kSwerve.Mod3.CONSTANTS)
+        };
+
+        SmartDashboard.putData("Field", field);
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+        SwerveModuleState[] swerveModuleStates = kSwerve.SWERVE_KINEMATICS.toSwerveModuleStates(fieldRelative
+                ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                        translation.getX(),
+                        translation.getY(),
+                        rotation,
+                        getYaw())
+                : new ChassisSpeeds(
+                        translation.getX(),
+                        translation.getY(),
+                        rotation));
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kSwerve.MAX_SPEED);
+
+        for (SwerveModule mod : mSwerveMods) {
+            mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+        }
+    }
+
+    /* Used by SwerveControllerCommand in Auto */
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, kSwerve.MAX_SPEED);
+
+        for (SwerveModule mod : mSwerveMods) {
+            mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+        }
+    }
+
+    public Pose2d getPose() {
+        return swerveOdometry.getPoseMeters();
+    }
+
+    public void resetOdometry(Pose2d pose) {
+        gyro.setYaw(pose.getRotation().getDegrees());
+        swerveOdometry.resetPosition(pose, pose.getRotation());
+    }
+
+    public SwerveModuleState[] getStates() {
+        SwerveModuleState[] states = new SwerveModuleState[4];
+        for (SwerveModule mod : mSwerveMods) {
+            states[mod.moduleNumber] = mod.getState();
+        }
+        return states;
+    }
+
+    public void zeroGyro() {
+        gyro.setYaw(0);
+    }
+
+    public Rotation2d getYaw() {
+        double[] ypr = new double[3];
+        gyro.getYawPitchRoll(ypr);
+        return (kSwerve.GYRO_INVERSION) ? Rotation2d.fromDegrees(360 - ypr[0]) : Rotation2d.fromDegrees(ypr[0]);
+    }
+
+    @Override
+    public void periodic() {
+        swerveOdometry.update(getYaw(), getStates());
+        field.setRobotPose(swerveOdometry.getPoseMeters());
+        SmartDashboard.putNumber("gyro", getYaw().getDegrees());
+
+        for (SwerveModule mod : mSwerveMods) {
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle", mod.getAngle());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Integrated", mod.getState().angle.getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);
+        }
+    }
+
+    public void updateEncoders() {
+        for (SwerveModule mod : mSwerveMods) {
+            mod.resetToAbsolute();
+        }        
+    }
+}
---
+++ src/main/java/frc/robot/util/SwerveModule.java
@@ -0,0 +1,133 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.Constants.kPorts;
+import frc.robot.Constants.kSwerve;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.WPI_CANCoder;
+
+import SushiFrcLib.SmartDashboard.TunableNumber;
+
+import java.util.PriorityQueue;
+
+public class SwerveModule {
+    public int moduleNumber;
+    private double angleOffset;
+    private TalonFX mAngleMotor;
+    private TalonFX mDriveMotor;
+    private CANCoder angleEncoder;
+    private double lastAngle;
+    private PriorityQueue<Double> cancoderAngles;
+
+    private final TunableNumber mDriveP;
+    private final TunableNumber mDriveD;
+    private final TunableNumber mDriveF;
+
+    public SwerveModule(int moduleNumber, SwerveModuleConstants moduleConstants) {
+        cancoderAngles = new PriorityQueue<Double>(5);
+        this.moduleNumber = moduleNumber;
+        angleOffset = moduleConstants.angleOffset;
+
+        /* Angle Encoder Config */
+        angleEncoder = new WPI_CANCoder(moduleConstants.cancoderID, kPorts.CANIVORE_NAME);
+        configAngleEncoder();
+
+        /* Angle Motor Config */
+        mAngleMotor = new WPI_TalonFX(moduleConstants.angleMotorID, kPorts.CANIVORE_NAME);
+        configAngleMotor();
+
+
+        /* Drive Motor Config */
+        mDriveMotor = new WPI_TalonFX(moduleConstants.driveMotorID, kPorts.CANIVORE_NAME);
+        configDriveMotor();
+
+        lastAngle = getState().angle.getDegrees();
+
+        mDriveP = new TunableNumber("Mod " + moduleNumber + " drive P", 0.0, Constants.TUNING_MODE);
+        mDriveD = new TunableNumber("Mod " + moduleNumber + " drive D", 0.0, Constants.TUNING_MODE);
+        mDriveF = new TunableNumber("Mod " + moduleNumber + " drive F", 0.0, Constants.TUNING_MODE);
+    }
+
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
+        if (mDriveP.hasChanged()) {
+            mDriveMotor.config_kP(0, mDriveP.get());
+        }
+        if (mDriveD.hasChanged()) {
+            mDriveMotor.config_kD(0, mDriveD.get());
+        }
+        if (mDriveF.hasChanged()) {
+            mDriveMotor.config_kF(0, mDriveF.get());
+        }
+
+        desiredState = CTREModuleState.optimize(desiredState, getState().angle); // Custom optimize command, since
+                                                                                 // default WPILib optimize assumes
+                                                                                 // continuous controller which CTRE is
+                                                                                 // not
+
+        if (isOpenLoop) {
+            double percentOutput = desiredState.speedMetersPerSecond / kSwerve.MAX_SPEED;
+            mDriveMotor.set(ControlMode.PercentOutput, percentOutput);
+        } else {
+            double velocity = Conversions.MPSToFalcon(desiredState.speedMetersPerSecond, kSwerve.WHEEL_CIRCUMFRANCE,
+                    kSwerve.DRIVE_GEAR_RATIO);
+            mDriveMotor.set(ControlMode.Velocity, velocity);
+        }
+
+        double angle = (Math.abs(desiredState.speedMetersPerSecond) <= (kSwerve.MAX_SPEED * 0.01)) ? lastAngle
+                : desiredState.angle.getDegrees(); // Prevent rotating module if speed is less then 1%. Prevents
+                                                   // Jittering.
+        mAngleMotor.set(ControlMode.Position, Conversions.degreesToFalcon(angle, kSwerve.ANGLE_GEAR_RATIO));
+        lastAngle = angle;
+    }
+
+    public void resetToAbsolute() {
+        double absolutePosition = Conversions.degreesToFalcon(getAngle(), kSwerve.ANGLE_GEAR_RATIO);
+        // for( Double d : )
+        mAngleMotor.setSelectedSensorPosition(absolutePosition);
+    }
+
+    private void configAngleEncoder() {
+        angleEncoder.configFactoryDefault();
+        angleEncoder.configAllSettings(Robot.ctreConfigs.swerveCanCoderConfig);
+    }
+
+    private void configAngleMotor() {
+        mAngleMotor.configFactoryDefault();
+        mAngleMotor.configAllSettings(Robot.ctreConfigs.swerveAngleFXConfig);
+        mAngleMotor.setInverted(kSwerve.ANGLE_INVERSION);
+        mAngleMotor.setNeutralMode(kSwerve.ANGLE_NEUTRAL_MODE);
+        resetToAbsolute();
+    }
+
+    private void configDriveMotor() {
+        mDriveMotor.configFactoryDefault();
+        mDriveMotor.configAllSettings(Robot.ctreConfigs.swerveDriveFXConfig);
+        mDriveMotor.setInverted(kSwerve.DRIVE_INVERSION);
+        mDriveMotor.setNeutralMode(kSwerve.DRIVE_NEUTRAL_MODE);
+        mDriveMotor.setSelectedSensorPosition(0);
+    }
+
+    public Rotation2d getCanCoder() {
+        return Rotation2d.fromDegrees(angleEncoder.getAbsolutePosition());
+    }
+
+    public double getAngle() {
+        return -getCanCoder().getDegrees() + angleOffset;
+    }
+
+    public SwerveModuleState getState() {
+        double velocity = Conversions.falconToMPS(mDriveMotor.getSelectedSensorVelocity(), kSwerve.WHEEL_CIRCUMFRANCE,
+                kSwerve.DRIVE_GEAR_RATIO);
+        Rotation2d angle = Rotation2d.fromDegrees(
+                Conversions.falconToDegrees(mAngleMotor.getSelectedSensorPosition(), kSwerve.ANGLE_GEAR_RATIO));
+        return new SwerveModuleState(velocity, angle);
+    }
+
+}
